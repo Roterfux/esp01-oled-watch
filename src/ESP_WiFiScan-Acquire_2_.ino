782503a93e0c600f6cb34aae25ebf71fa1721a68
Roterfux <jgreimel@gmail.com>
2017-06-19 23:26:53 +0200

    simplyfied code better update function shorter startup removed unused code

--- src/ESP_WiFiScan-Acquire_2_.ino
+++ src/ESP_WiFiScan-Acquire_2_.ino
@@ -1,9 +1,11 @@
 #include "ESP8266WiFi.h"
 #include <WiFiUdp.h>
 #include <Wire.h>
+#include <U8g2lib.h>
+#include <SPI.h>
 
 char buffer[20];
-char *password         = "...";
+char *password         = "JuV30062013";
 char *ssid             = "Fuxbau";
 String MyNetworkSSID   = "Fuxbau"; // SSID you want to connect to Same as SSID
 bool Fl_MyNetwork      = false;       // Used to flag specific network has been found
@@ -20,7 +22,7 @@ byte packetBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing
                                     // packets
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udp;
-const int interval  = 100;
+int reconnect_interval  = 30;
 int count           = 0;
 unsigned long epoch = 0;
 
@@ -28,218 +30,159 @@ extern "C" {
     #include "user_interface.h"
 }
 
+
 void setup() {
     Serial.begin(115200);
-    delay(2000); // wait for uart to settle and print Espressif blurb..
+    delay(2500); // wait for uart to settle and print Espressif blurb..
     // Wire.pins(int sda, int scl), etc
     Wire.pins(0, 2); // on ESP-01.
     Wire.begin();
     StartUp_OLED(); // Init Oled and fire up!
-    Serial.println("OLED Init...");
     clear_display();
-    //setXY(6, 1);
-    //sendStr("START-UP ...  ");
-    drawBigText();
-    delay(1000);
-    Serial.println("Setup done");
-    clear_display(); // Clear OLED
+    setXY(6, 1);
+    sendStr("Start up!");
     udp.begin(localPort);
     Scan_Wifi_Networks();
     Do_Connect();
     epoch = ntpRequest();
+    clear_display();
 }
 
 void loop() {
   if (!Fl_NetworkUP) {
-    Serial.println("Starting Process Scanning...");
-    sendStrXY("SCANNING ...", 0, 1);
     Scan_Wifi_Networks();
     if (Fl_MyNetwork) {
-      // Yep we have our network lets try and connect to it..
-      Serial.println(
-          "MyNetwork has been Found... attempting Connection to Network...");
-      Do_Connect();
-      if (Fl_NetworkUP) {
-        // Connection success
-        Serial.println("Connected OK");
-        clear_display();               // Clear OLED
-        IPAddress ip = WiFi.localIP(); // // Convert IP Here
-        String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' +
-                       String(ip[2]) + '.' + String(ip[3]);
-        ipStr.toCharArray(buffer, 20);
-      }
-      else
-      {
-        // Connection failure
-        Serial.println("Not Connected");
-        clear_display();                   // Clear OLED
-        sendStrXY("Not connected!", 4, 1); // YELLOW LINE DISPLAY
-        //delay(1000);
-      }
+        Do_Connect();
+        if (Fl_NetworkUP) { }
+        else{
+            // Connection failure
+            clear_display();                   // Clear OLED
+            sendStrXY("Not connected!", 4, 1); // YELLOW LINE DISPLAY
+        }
     }
-    else
-    {
-      // Nope my network not identified in Scan
-      Serial.println("Not Connected");
-      clear_display(); // Clear OLED
-      sendStrXY("Not connected!", 4, 1);
-      //delay(1000);
+    else {
+        // Nope my network not identified in Scan
+        clear_display(); // Clear OLED
+        sendStrXY("Not connected!", 4, 1);
     }
   }
-  if (count > interval) {
+
+  if (count > reconnect_interval) {
         unsigned long time_update = ntpRequest();
         if(time_update > 0) {
+            reconnect_interval = 30;
             epoch = time_update;
+            sendStrXY("?", 0, 0);
         } else {
-            epoch = epoch + 5;
+            epoch = epoch + 6;
+            // try more often
+            reconnect_interval = 300;
+            sendStrXY("!", 0, 0);
         }
     count = 0;
-  }
-  else
-  {
-    epoch++;
-    count++;
-  }
-  printTime(epoch, 1);
-  printTime(epoch, 2);
-  printTime(epoch, 3);
-  delay(1000); // Wait a little before trying again
+    }
+    else {
+        epoch++;
+        count++;
+    }
+    printTime(1);
+    printTime(2);
+    printTime(3);
+    delay(1050); // Wait a little before trying again
 }
 
 unsigned long ntpRequest() {
-  sendStrXY("Request Time...", 0, 0);
-  // get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP);
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(2000);
-  clear_display(); // Clear OLED
-  int cb = udp.parsePacket();
-  if (!cb) {
+    sendStrXY("Request Time...", 0, 0);
+    // get a random server from the pool
+    WiFi.hostByName(ntpServerName, timeServerIP);
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    // wait to see if a reply is available
+    delay(2500);
     clear_display(); // Clear OLED
-    Serial.println("no packet yet");
-    sendStrXY("No Wifi connect.", 0, 0);
-    return 0;
-  }
-  else
-  {
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-    // the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord  = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-    // print the hour, minute and second:
-    clear_display(); // Clear OLED
-    // --- HOURS ---
-    printTime(epoch, 1);
-    printTime(epoch, 2);
-    printTime(epoch, 3);
-    return epoch;
-  }
+    int cb = udp.parsePacket();
+    if (!cb) {
+        sendStrXY("No Wifi connect.", 0, 0);
+        return 0;
+    }
+    else {
+        // We've received a packet, read the data from it
+        udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+        // the timestamp starts at byte 40 of the received packet and is four bytes,
+        // or two words, long. First, esxtract the two words:
+        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+        unsigned long lowWord  = word(packetBuffer[42], packetBuffer[43]);
+        // combine the four bytes (two words) into a long integer
+        // this is NTP time (seconds since Jan 1 1900):
+        unsigned long secsSince1900 = highWord << 16 | lowWord;
+        // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+        const unsigned long seventyYears = 2208988800UL;
+        // subtract seventy years:
+        epoch = secsSince1900 - seventyYears;
+    }
 }
 
 // send an NTP request to the time server at the given address
 unsigned long sendNTPpacket(IPAddress &address) {
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011; // LI, Version, Mode
-  packetBuffer[1] = 0;          // Stratum, or type of clock
-  packetBuffer[2] = 6;          // Polling Interval
-  packetBuffer[3] = 0xEC;       // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12] = 49;
-  packetBuffer[13] = 0x4E;
-  packetBuffer[14] = 49;
-  packetBuffer[15] = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); // NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
+    Serial.println("sending NTP packet...");
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011; // LI, Version, Mode
+    packetBuffer[1] = 0;          // Stratum, or type of clock
+    packetBuffer[2] = 6;          // Polling Interval
+    packetBuffer[3] = 0xEC;       // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    udp.beginPacket(address, 123); // NTP requests are to port 123
+    udp.write(packetBuffer, NTP_PACKET_SIZE);
+    udp.endPacket();
 }
 
-void printTime(unsigned long epoch, int kind) {
-  char display_buffer_hours[4];
-  char display_buffer_minutes[4];
-  char display_buffer_seconds[4];
-  char pos_y = 2;
-  setXY(pos_y, 6);
-  sendStr("TIME");
-  pos_y = 4;
+void printTime(int kind) {
+    char pos_y = 4;
 
-  if (kind == 1) {
-    // --- HOURS ---
-    int calc = (((epoch % 86400L) / 3600) + 2);
-    int hours;
-    if (calc == 25) { hours = 1; } else { hours = calc; }
+    if (kind == 1) {
+        // Hours
+        int calc = (((epoch % 86400L) / 3600) + 2);
+        int hours;
+        if (calc >= 24) hours = calc - 24; else hours = calc;
 
-    String display_time_hours = String(hours);
-    display_time_hours.toCharArray(display_buffer_hours, 4);
-    Serial.print((epoch % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    setXY(pos_y, 4);
-    if (hours < 10) {
-      sendStr("0");
-      sendStr(display_buffer_hours);
-    } else
-    { sendStr(display_buffer_hours); }
-    sendStr(":");
-  }
-
-  if (kind == 2)
-  {
-     int calc = (epoch % 3600) / 60;
-    // Minutes
-    String display_time_minutes = String(calc);
-    display_time_minutes.toCharArray(display_buffer_minutes, 4);
-    Serial.print(calc); // print the minute (3600 equals secs per minute)
-    setXY(pos_y, 7);
-    if ((epoch % 60) < 10) {
-      sendStr("0");
-      sendStr(display_buffer_minutes);
-    } else
-    { sendStr(display_buffer_minutes); }
-    sendStr(":");
-  }
-
-  if (kind == 3)
-  {
-    // Seconds
-    String display_time_seconds = String(epoch % 60);
-    display_time_seconds.toCharArray(display_buffer_seconds, 4);
-    Serial.println(epoch % 60); // print the second
-    setXY(pos_y, 10);
-    if (epoch < 10) {
-      sendStr("0");
-      sendStr(display_buffer_seconds);
-    } else
-    { sendStr(display_buffer_seconds); }
-  }
-  setXY(0, 0);
+        setXY(pos_y, 4);
+        if (hours < 10)
+            sendStr("0");
+        drawConvertedText(hours);
+        sendStr(":");
+    }
+    if (kind == 2) {
+        int calc = (epoch % 3600) / 60;
+        // Minutes
+        setXY(pos_y, 7);
+        if (calc < 10)
+            sendStr("0");
+        drawConvertedText(calc);
+        sendStr(":");
+    }
+    if (kind == 3) {
+        // Seconds
+        int calc = epoch % 60;
+        setXY(pos_y, 10);
+        if (calc < 10)
+            sendStr("0");
+        drawConvertedText(calc);
+    }
+    setXY(0, 0);
 }
 
-
-void drawBigText()
+void drawConvertedText(unsigned long value)
 {
-    sendStrXY("000", 0, 0);
-    sendStrXY("0 0", 1, 0);
-    sendStrXY("0 0", 2, 0);
-    sendStrXY("0 0", 3, 0);
-    sendStrXY("000", 4, 0);
+    char display_buffer[4];
+    String display_text = String(value);
+    display_text.toCharArray(display_buffer, 4);
+    sendStr(display_buffer);
 }
